2274cdf537c9bf66485232e8b6021e86691834fd
tekihei2317 <tekihei2317@gmail.com>
2022-04-28 07:04:45 +0900

    feat: 新刊情報を取得する関数を実装

---
+++ functions/src/firestore-admin/feed-memo.ts
@@ -0,0 +1,31 @@
+import admin from 'firebase-admin'
+
+import { collectionName } from '../services/mangarel/constants'
+import { FeedMemo } from '../services/mangarel/models/feed-memo'
+
+export const saveFeedMemo = async (
+  db: admin.firestore.Firestore,
+  memos: FeedMemo[],
+  publisher: string
+) => {
+  const memosRef = db.collection(collectionName.feedMemos)
+  const query = await memosRef.where('publisher', '==', publisher).get()
+  const existingMemos = query.docs.map((doc) => doc.data() as FeedMemo)
+
+  let newMemoCount = 0
+  for await (const memo of memos) {
+    if (existingMemos.some((m) => m.title === memo.title)) {
+      continue
+    }
+
+    await memosRef.doc().set({
+      ...memo,
+      fetchedAt: admin.firestore.Timestamp.fromDate(new Date(0)),
+      updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+    })
+
+    newMemoCount++
+  }
+
+  return newMemoCount
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,43 @@
-// import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
+import admin from 'firebase-admin'
+import puppeteer from 'puppeteer'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { collectionName } from './services/mangarel/constants'
+import { feedCalendar } from './crawlers/kodansha-calendar'
+import { saveFeedMemo } from './firestore-admin/feed-memo'
+
+admin.initializeApp()
+
+export const publishers = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
+  const snapshot = await admin.firestore().collection(collectionName.publishers).get()
+
+  const data = snapshot.docs.map((doc) => doc.data())
+  res.send({ data })
+})
+
+const PUPPETEER_OPTIONS = {
+  args: [
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+    '--single-process',
+  ],
+  headless: true,
+}
+
+export const fetchCalendar = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS)
+    const page = await browser.newPage()
+
+    const db = admin.firestore()
+    const feedMemos = await feedCalendar(page)
+    const newMemoCount = await saveFeedMemo(db, feedMemos, 'kodansha')
+    await browser.close()
+
+    res.send(`Fetched Kodansha calendar. Wrote ${newMemoCount} memos.`)
+  })
